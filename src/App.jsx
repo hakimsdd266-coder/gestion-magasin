cd206ff956335dc607ca7885340ec6f38b89f0b7
hakimsdd266-coder <hakimsdd266@gmail.com>
2026-08-13 22:12:04 +0100

    Delete src/App.jsx

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <h1 className="text-2xl font-semibold text-blue-600">App prête</h1>
-    </div>
-  )
-}
-
-export default App
